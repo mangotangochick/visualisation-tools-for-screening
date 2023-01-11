487c692c18778e5a54526ad2705658c3bb7f63be
Alistair James <alistair.james@students.plymouth.ac.uk>
2023-01-11 12:29:37 +0000

    Added tests

--- visualisation_package/testing/test_dataset.py
+++ visualisation_package/testing/test_dataset.py
@@ -3,6 +3,7 @@ Testing the built-in datasets.
 '''
 import pytest
 from vis_tools import datasets as ds
+from vis_tools import visualisation
 
 
 def test_col_pres(cancer_type):
@@ -19,6 +20,15 @@ def test_col_pres(cancer_type):
         data = ds.load_cerv()
     df_columns = list(data.columns)
     assert columns in df_columns == True
+
+def test_region_analysis_year_setting():
+    region_test = visualisation.Region_Analysis(2016, False)
+    assert region_test.year == 2016
+    
+def test_get_geoshape_info_from_api_request_for_areacode():
+    region_test = visualisation.Region_Analysis(2016, False)
+    shape, was_error = region_test.get_geoshape_info_from_api_request_for_areacode('E12000001')
+    assert was_error == False
 
 
 test_col_pres('cervical')

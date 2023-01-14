9bea7b503e6da82f4a74ad76d2f29d819a68c2b1
mangotangochick <99885127+mangotangochick@users.noreply.github.com>
2023-01-14 11:19:36 +0000

    demo changes

--- visualisation_package/vis_tools/deprivation.py
+++ visualisation_package/vis_tools/deprivation.py
@@ -1,4 +1,5 @@
 import os
+from matplotlib import patches
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 from datasets import *
 
@@ -54,7 +55,8 @@ class DeprivationPlots():
             ax[1].set_facecolor('#C8C8A9')
             ax[0].set_title(f'Percentage Uptake for Most Deprived Deciles in the year {year}')
             ax[1].set_title(f'Percentage Uptake for Least Deprived Deciles in the {year}')
-            
+
+
             for p in ax[0].patches:
                 ax[0].annotate(
                     str(p.get_height().round(1)) + '%',
@@ -70,6 +72,7 @@ class DeprivationPlots():
                     textcoords='offset points')
 
             for ax in [ax[0], ax[1]]:
+                
                 for i, rect in enumerate(ax.patches):
                     if df_most['Category Type'].str.contains('County').iloc[i]:
                         rect.set_facecolor('#FE4365')
@@ -79,6 +82,8 @@ class DeprivationPlots():
                         rect.set_facecolor('#FE4365')
                     elif df_least['Category Type'].str.contains('District').iloc[i]:
                         rect.set_facecolor('#83AF9B')
+                        
+                ax.legend(handles=[patches.Rectangle((0,0),1,1,facecolor='#FE4365', label='County'), patches.Rectangle((0,0),1,1,facecolor='#83AF9B', label='District')], loc=1)
 
-        plt.show()
-
+                        
+    
